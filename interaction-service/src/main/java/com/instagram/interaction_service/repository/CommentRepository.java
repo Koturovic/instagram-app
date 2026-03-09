20d05150debd838b8d68867fbc33a1f70f4a59d5
Nemanja Gligorov <gligorov70.ng@gmail.com>
2026-03-09 12:36:50 +0100

    Delete interaction-service directory

--- interaction-service/src/main/java/com/instagram/interaction_service/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.instagram.interaction_service.repository;
-
-import com.instagram.interaction_service.entity.Comment; // Ovo je ključno!
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPostId(Long postId);
-}
